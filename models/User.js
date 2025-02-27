676c8fdf678a959532ba392c5873f8aef4a890f4
rasersharpe <jay.bhatt@me.com>
2025-02-27 17:18:39 -0600

    Bug fixes

--- models/User.js
+++ models/User.js
@@ -1,45 +1,45 @@
 const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            match: [/.+@.+\..+/, "Please enter a valid email address"],
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "Thought"
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "User"
-            },
-        ],
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        }
-        id: false,
-    }
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 // Virtual for friend count
 UserSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
-  });
-  
-  const User = model("User", UserSchema);
-  
-  module.exports = User;
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Import routes
+const userRoutes = require("./routes/api/userRoutes");
+const thoughtRoutes = require("./routes/api/thoughtRoutes");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use API routes
+app.use("/api/users", userRoutes);
+app.use("/api/thoughts", thoughtRoutes);
+
+// Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/socialNetworkDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Log MongoDB queries being executed
 mongoose.set("debug", true);
 
-app.use(require("./routes"));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Server running on port ${PORT}`));
